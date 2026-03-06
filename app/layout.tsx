3e1fd8dd60e489cd6e0ccbe09ed91706a997af9c
nizarsyahmi37 <nizarsyahmi37@gmail.com>
2026-03-06 14:47:38 +0800

    init(app): Initialize Next.js setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import { ReactNode } from "react";
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+	title: "Superteam Malaysia",
+	description: "Superteam Malaysia"
+};
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={`antialiased`}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
